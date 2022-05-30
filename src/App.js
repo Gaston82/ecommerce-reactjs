1763ec8061483035489fa5d332e85fe93352b4f7
Gaston82 <cabezaartaza@hotmail.com>
2022-05-30 04:53:50 +0200

    modify image product size

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Routes, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Home from "./pages/home";
+import Detail from "./pages/detail";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Ecommerce </h1>
-      </header>
+        <h1>React Ecommerce </h1> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
+      </Routes>
+      {/* </header> */}
     </div>
   );
 }
--- src/components/product.js
+++ src/components/product.js
@@ -1,7 +1,7 @@
 export default function Product({ id, image, title }) {
   return (
     <article style={{ color: "black" }}>
-      <img src={image} alt={title} />
+      <img style={{ maxWidth: "300px" }} src={image} alt={title} />
       <h3>{title}</h3>
     </article>
   );
