7f16da324921a6136cba6ea0336530e2550eeaef
Gaston82 <cabezaartaza@hotmail.com>
2022-06-02 00:01:00 +0200

    add products component

--- src/components/product.js
+++ src/components/product.js
@@ -1,8 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Product({ id, image, title }) {
   return (
     <article style={{ color: "black" }}>
-      <img style={{ maxWidth: "300px" }} src={image} alt={title} />
-      <h3>{title}</h3>
+      <Link to={`/detail/${id}`}>
+        <img style={{ maxWidth: "200px" }} src={image} alt={title} />
+        <h3>{title}</h3>
+      </Link>
     </article>
   );
 }
