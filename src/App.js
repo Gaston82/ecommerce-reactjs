4ed8f7a8bf4053aedc5698bc8ca1b0f503555dfa
Gaston82 <cabezaartaza@hotmail.com>
2022-06-02 00:06:53 +0200

    add detail and home  routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Routes, Route, Link } from "react-router-dom";
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/home";
 import Detail from "./pages/detail";
@@ -7,14 +6,10 @@ import Detail from "./pages/detail";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>React Ecommerce </h1> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
       </Routes>
-      {/* </header> */}
     </div>
   );
 }
